bae9528938829986ed5292db48eed067b7b293ff
Vojtěch Zicha <mail@vojtechzicha.com>
2019-08-27 18:37:01 +0200

    ADD: Support for Minecraft Cloud saves

---
+++ game-save-locations.sh
@@ -0,0 +1,4 @@
+# Minecraft
+
+rm -rf ~/Library/Application\ Support/minecraft/saves
+n -s ~/Library/Mobile\ Documents/com\~apple\~CloudDocs/Games/Minecraft/saves ~/Library/Application\ Support/minecraft/saves
